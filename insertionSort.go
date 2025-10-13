26699daef5386e6b669d6b6e09c8e85028d23a2a
Aung Pyae Phyo <115131869+theaungphyo@users.noreply.github.com>
2025-10-13 11:27:26 +0630

    Update insertionSort.go

--- insertionSort.go
+++ insertionSort.go
@@ -5,13 +5,28 @@ import "fmt"
 func main() {
 	arr := []int{9, 5, 3, 4, 1, 11, 12, 6, 2}
 	sortArr := InsertionSort(arr)
+
+	sortDescArr := InsertionSortDesc(arr)
+
 	fmt.Println("Sorted array:", sortArr)
+	fmt.Println("Sorted desc array:", sortDescArr)
 }
 
 func InsertionSort(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
 		j := i
 		for j > 0 && arr[j] < arr[j-1] {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+			j--
+		}
+	}
+	return arr
+}
+
+func InsertionSortDesc(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		j := i
+		for j > 0 && arr[j] > arr[j-1] {
 			arr[j], arr[j-1] = arr[j-1], arr[j]
 			j--
 		}
