f4fbc74361344adda9aea7b5341c368b7874e405
Aung Pyae Phyo <115131869+theaungphyo@users.noreply.github.com>
2025-10-13 15:08:27 +0630

    Create delimeter.go

---
+++ delimeter.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("abc[", DelimeterMatch("abc[]}"))
+}
+
+func DelimeterMatch(str string) bool {
+	var stack []byte
+	for i := 0; i < len(str); i++ {
+		if str[i] == '[' {
+			stack = append(stack, str[i])
+		} else if str[i] == ']' {
+			if len(stack) == 0 {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else if str[i] == '{' {
+			stack = append(stack, str[i])
+		} else if str[i] == '}' {
+			if len(stack) == 0 {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	fmt.Println("Stack", stack)
+	return len(stack) == 0
+}
