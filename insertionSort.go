dcf609ee6710d8eaa3ec7917e55cb5b320acb9c0
Aung Pyae Phyo <115131869+theaungphyo@users.noreply.github.com>
2025-10-13 11:24:01 +0630

    Update insertionSort.go

--- insertionSort.go
+++ insertionSort.go
@@ -1,1 +1,20 @@
+package main
 
+import "fmt"
+
+func main() {
+	arr := []int{9, 5, 3, 4, 1, 11, 12, 6, 2}
+	sortArr := InsertionSort(arr)
+	fmt.Println("Sorted array:", sortArr)
+}
+
+func InsertionSort(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		j := i
+		for j > 0 && arr[j] < arr[j-1] {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+			j--
+		}
+	}
+	return arr
+}
